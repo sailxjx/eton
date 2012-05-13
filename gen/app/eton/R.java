386205b76f9de66a2d2d671f1ba66049e7ce41be
sailxjx <sailxjx@gmail.com>
2012-05-13 20:09:17 +0800

    add ignore;

--- gen/app/eton/R.java
+++ gen/app/eton/R.java
@@ -19,19 +19,17 @@ public final class R {
         public static final int editText2=0x7f050003;
         public static final int textView1=0x7f050000;
         public static final int textView2=0x7f050002;
-        public static final int textView3=0x7f050005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int btnConfirm=0x7f040007;
-        public static final int hello=0x7f040000;
-        public static final int hintHeight=0x7f040006;
-        public static final int hintWeight=0x7f040005;
-        public static final int labelHeight=0x7f040002;
-        public static final int labelTitle=0x7f040004;
-        public static final int labelWeight=0x7f040003;
+        public static final int app_name=0x7f040000;
+        public static final int btnConfirm=0x7f040006;
+        public static final int hintHeight=0x7f040005;
+        public static final int hintWeight=0x7f040004;
+        public static final int labelHeight=0x7f040001;
+        public static final int labelTitle=0x7f040003;
+        public static final int labelWeight=0x7f040002;
     }
 }
